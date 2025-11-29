509c433fbbd851c4320fdc40c4e4437ec6c543c5
Farris Attiya <farrisattiya@Farriss-MacBook-Air.local>
2025-11-29 17:34:56 -0500

    fix bugs

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,5 +1,3 @@
-# src/dashboard.py
-
 import os
 from datetime import datetime
 
@@ -7,7 +5,7 @@ import psycopg2
 from flask import Flask, render_template
 from dotenv import load_dotenv
 
-load_dotenv()  # load .env from project root
+load_dotenv() 
 
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
